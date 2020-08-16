b1b8fea5bb7eddf23a1d2b67d4325904407083e4
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-08-16 13:50:24 -0300

    home-about

--- pet-care/src/components/pages/home/pageHome.jsx
+++ pet-care/src/components/pages/home/pageHome.jsx
@@ -3,6 +3,7 @@ import './pageHome.css'
 
 import Button from '../../button/Button'
 const imgTitle = require('../../../assets/img-title.png')
+const imgAbout = require('../../../assets/img-vet.png')
 
 
 export default function PageHome() {
@@ -16,12 +17,24 @@ export default function PageHome() {
                         <Button>AGENDE UMA VISITA</Button>
                     </div>
                     <div className='img-title'>
-                        <img src={imgTitle} alt=""/>
+                        <img src={imgTitle} alt="Cachorrinho" />
                     </div>
                 </div>
 
                 <div className='home-about'>
-                        sdfsdfv vv
+                    <div className='img-about'>
+                        <img src={imgAbout} alt="vet" />
+                    </div>
+                    <div className='title-about'>
+                        <h2>Sobre mim</h2>
+                        <p>Sou um parágrafo. Clique aqui para editar e 
+                            adicionar o seu próprio texto. É fácil! 
+                            Basta clicar em "Editar Texto" ou clicar 
+                            duas vezes sobre mim e você poderá adicionar 
+                            o seu próprio conteúdo e trocar fontes. 
+                            Sou um ótimo lugar para você contar sua história</p>
+                        <Button>LEIA MAIS</Button>
+                    </div>
                 </div>
             </main>
         </>
