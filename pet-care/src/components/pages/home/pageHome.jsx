99cd640a219d98fa2dbb5e525a8a0ee17bd091bd
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-08-18 12:38:29 -0300

    home-services

--- pet-care/src/components/pages/home/pageHome.jsx
+++ pet-care/src/components/pages/home/pageHome.jsx
@@ -7,6 +7,8 @@ import Services from '../../services/Services'
 const imgTitle = require('../../../assets/img-title.png')
 const imgAbout = require('../../../assets/img-vet.png')
 const imgServiceBrushing = require('../../../assets/escovacao.png')
+const imgServiceCheckup = require('../../../assets/checkup.png')
+const imgServiceDental = require('../../../assets/dental.png')
 
 
 export default function PageHome() {
@@ -54,7 +56,7 @@ export default function PageHome() {
                         </div>
 
                         <div className='card-services'>
-                            <Services imageService={imgServiceBrushing} nameOfService='Escovação'
+                            <Services imageService={imgServiceCheckup} nameOfService='Checkup'
                                 descriptionOfService='Sou um parágrafo. Use esta área para descrever 
                     um de seus serviços. Você pode trocar o título para o serviço que você 
                     presta e descrevê-lo. Sinta-se à vontade para mudar a imagem.'
@@ -63,13 +65,13 @@ export default function PageHome() {
                         </div>
 
                         <div className='card-services'>
-                            <Services imageService={imgServiceBrushing} nameOfService='Escovação'
+                            <Services imageService={imgServiceDental} nameOfService='Dental'
                                 descriptionOfService='Sou um parágrafo. Use esta área para descrever 
                     um de seus serviços. Você pode trocar o título para o serviço que você 
                     presta e descrevê-lo. Sinta-se à vontade para mudar a imagem.'
                             />
                             <Button>RESERVE JÁ</Button>
-                        </div>  z
+                        </div>  
                     </div>
 
 
