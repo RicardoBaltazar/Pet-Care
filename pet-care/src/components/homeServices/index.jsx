b85bde431c014271c3f1d09c8f65b18ed630a6c1
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-08-19 13:20:35 -0300

    criando componente homeServices e refatorando pageHome

---
+++ pet-care/src/components/homeServices/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './homeServices.css'
+import Button from '../button/Button'
+import Services from '../services/Services'
+const imgServiceBrushing = require('../../assets/escovacao.png')
+const imgServiceCheckup = require('../../assets/checkup.png')
+const imgServiceDental = require('../../assets/dental.png')
+
+export default function HomeServices() {
+    return (
+        <div className='home-services'>
+            <h2>Serviços</h2>
+
+            <div className='cards-services'>
+                <div className='card-services'>
+                    <Services imageService={imgServiceBrushing} nameOfService='Escovação'
+                        descriptionOfService='Sou um parágrafo. Use esta área para descrever 
+        um de seus serviços. Você pode trocar o título para o serviço que você 
+        presta e descrevê-lo. Sinta-se à vontade para mudar a imagem.'
+                    />
+                    <Button>RESERVE JÁ</Button>
+                </div>
+
+                <div className='card-services'>
+                    <Services imageService={imgServiceCheckup} nameOfService='Checkup'
+                        descriptionOfService='Sou um parágrafo. Use esta área para descrever 
+        um de seus serviços. Você pode trocar o título para o serviço que você 
+        presta e descrevê-lo. Sinta-se à vontade para mudar a imagem.'
+                    />
+                    <Button>RESERVE JÁ</Button>
+                </div>
+
+                <div className='card-services'>
+                    <Services imageService={imgServiceDental} nameOfService='Dental'
+                        descriptionOfService='Sou um parágrafo. Use esta área para descrever 
+        um de seus serviços. Você pode trocar o título para o serviço que você 
+        presta e descrevê-lo. Sinta-se à vontade para mudar a imagem.'
+                    />
+                    <Button>RESERVE JÁ</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pet-care/src/components/pages/home/pageHome.jsx
+++ pet-care/src/components/pages/home/pageHome.jsx
@@ -3,15 +3,12 @@ import './pageHome.css'
 
 import HomeTitle from '../../homeTitle/index'
 import HomeAbout from '../../homeAbout/index'
-import Button from '../../button/Button'
-import Services from '../../services/Services'
+import HomeServices from '../../homeServices/index'
 import ImageClients from '../../imageClients/index'
 import DepositionsClients from '../../depositionsClients/index'
 import Form from '../../form/Form'
 
-const imgServiceBrushing = require('../../../assets/escovacao.png')
-const imgServiceCheckup = require('../../../assets/checkup.png')
-const imgServiceDental = require('../../../assets/dental.png')
+
 
 
 export default function PageHome() {
@@ -20,42 +17,7 @@ export default function PageHome() {
             <main>
                 <HomeTitle  />
                 <HomeAbout />
-
-
-
-                <div className='home-services'>
-                    <h2>Serviços</h2>
-
-                    <div className='cards-services'>
-                        <div className='card-services'>
-                            <Services imageService={imgServiceBrushing} nameOfService='Escovação'
-                                descriptionOfService='Sou um parágrafo. Use esta área para descrever 
-                    um de seus serviços. Você pode trocar o título para o serviço que você 
-                    presta e descrevê-lo. Sinta-se à vontade para mudar a imagem.'
-                            />
-                            <Button>RESERVE JÁ</Button>
-                        </div>
-
-                        <div className='card-services'>
-                            <Services imageService={imgServiceCheckup} nameOfService='Checkup'
-                                descriptionOfService='Sou um parágrafo. Use esta área para descrever 
-                    um de seus serviços. Você pode trocar o título para o serviço que você 
-                    presta e descrevê-lo. Sinta-se à vontade para mudar a imagem.'
-                            />
-                            <Button>RESERVE JÁ</Button>
-                        </div>
-
-                        <div className='card-services'>
-                            <Services imageService={imgServiceDental} nameOfService='Dental'
-                                descriptionOfService='Sou um parágrafo. Use esta área para descrever 
-                    um de seus serviços. Você pode trocar o título para o serviço que você 
-                    presta e descrevê-lo. Sinta-se à vontade para mudar a imagem.'
-                            />
-                            <Button>RESERVE JÁ</Button>
-                        </div>  
-                    </div>
-                </div>
-
+                <HomeServices />
                 <ImageClients />
                 <DepositionsClients />
                 <Form />
