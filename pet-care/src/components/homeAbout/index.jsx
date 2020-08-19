dbf6c2a4347ef9b05338663ed2deb57495e5f870
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-08-19 14:37:49 -0300

    criando responsividade do componente homeAbout

--- pet-care/src/components/homeAbout/index.jsx
+++ pet-care/src/components/homeAbout/index.jsx
@@ -7,19 +7,19 @@ const imgAbout = require('../../assets/img-vet.png')
 export default function HomeAbout() {
     return (
         <div className='home-about'>
-        <div className='img-about'>
-            <img src={imgAbout} alt="vet" />
-        </div>
-        <div className='title-about'>
-            <h2>Sobre mim</h2>
-            <p>Sou um parágrafo. Clique aqui para editar e
-            adicionar o seu próprio texto. É fácil!
-            Basta clicar em "Editar Texto" ou clicar
-            duas vezes sobre mim e você poderá adicionar
-            o seu próprio conteúdo e trocar fontes.
+            <div className='img-about'>
+                <img src={imgAbout} alt="vet" />
+            </div>
+            <div className='title-about'>
+                <h2>Sobre mim</h2>
+                <p>Sou um parágrafo. Clique aqui para editar e
+                adicionar o seu próprio texto. É fácil!
+                Basta clicar em "Editar Texto" ou clicar
+                duas vezes sobre mim e você poderá adicionar
+                o seu próprio conteúdo e trocar fontes.
                 Sou um ótimo lugar para você contar sua história</p>
-            <Button>LEIA MAIS</Button>
+                <Button>LEIA MAIS</Button>
+            </div>
         </div>
-    </div>
     )
 }
