2bbd36a3cae5cd66a012751c16941f12f98f67c3
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-08-28 14:44:18 -0300

    feature - componente pageServices

--- pet-care/src/components/pages/services/index.jsx
+++ pet-care/src/components/pages/services/index.jsx
@@ -3,6 +3,11 @@ import './services.css'
 import CardsPageServices from '../../cardsPageServices/index'
 
 const img1 = require('../../../assets/img-services1.png')
+const img2 = require('../../../assets/img-services2.png')
+const img3 = require('../../../assets/img-services3.png')
+const img4 = require('../../../assets/img-services4.png')
+const img5 = require('../../../assets/img-services5.png')
+const img6 = require('../../../assets/img-services6.png')
 
 export default function PageServices() {
     return (
@@ -19,26 +24,48 @@ export default function PageServices() {
                 time="35 min"
                 value='R$35'
                 />
-                
+
                 <CardsPageServices 
-                img={img1}
-                titleService="Escovação"
+                img={img2}
+                titleService="Vacinação"
                 descriptionService="Use esta área para descrever o seu serviço."
-                time="35 min"
-                value='R$35'
+                time="15 min"
+                value='R$60'
                 />
 
                 <CardsPageServices 
-                img={img1}
-                titleService="Escovação"
+                img={img3}
+                titleService="Consulta Comportamental"
                 descriptionService="Use esta área para descrever o seu serviço."
-                time="35 min"
-                value='R$35'
+                time="1 h 30 min"
+                value='R$200'
                 />
             </section>
 
             <section>
-                asdasd
+            <CardsPageServices 
+                img={img4}
+                titleService="Checkup"
+                descriptionService="Use esta área para descrever o seu serviço."
+                time="30 min"
+                value='R$100'
+                />
+                
+                <CardsPageServices 
+                img={img5}
+                titleService="Terapia para Dores"
+                descriptionService="Use esta área para descrever o seu serviço."
+                time="45 min"
+                value='R$45'
+                />
+
+                <CardsPageServices 
+                img={img6}
+                titleService="Cuidado Dental"
+                descriptionService="Use esta área para descrever o seu serviço."
+                time="20 min"
+                value='R$25'
+                />
             </section>
         </div>
     )
